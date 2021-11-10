c896d97097abe364efa98eb7fc1ea75bf51b591a
Julien Wolfensberger <jwolfensb@ethz.ch>
2021-11-10 18:35:37 +0100

    account class

--- lib/account.dart
+++ lib/account.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Account {
-  Account(
-      {this.title = "", image, this.statements = const [], this.label = ""});
+  Account({
+    this.title = "",
+    image,
+    this.label = "",
+    this.statementsfrom = const [],
+    this.statementsabout = const [],
+    this.statementsrelevant = const [],
+  });
 
   String title;
   Image? image;
-  List<String> statements;
   String label;
+  List<String> statementsfrom;
+  List<String> statementsabout;
+  List<String> statementsrelevant;
 }
