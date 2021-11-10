8cd80089544c3d6985dff0ca6511faa80386996f
Julien Wolfensberger <jwolfensb@ethz.ch>
2021-11-10 18:30:25 +0100

    implement autocomplete

--- lib/main.dart
+++ lib/main.dart
@@ -53,11 +53,26 @@ class _MyHomePageState extends State<MyHomePage> {
     Account(title: 'Joe Biden', label: 'Politician'),
     Account(title: 'Apple', label: 'Company'),
     Account(title: 'Apple', label: 'Fruit'),
-    Account(title: 'Armin Planincic', label: 'Legend'),
+    Account(title: 'Nicky Minaj', label: 'Singer'),
+    Account(title: 'Benjamin Shapiro', label: 'Author'),
+    Account(title: 'Jordan Peterson', label: 'Author'),
   ];
 
-  List<Account> getSuggestions() {
-    return suggestions;
+  List<Account> getSuggestions(String pattern) {
+    //If the user has not given any input yet, return nothing it all
+    if (pattern == "") {
+      return [];
+    }
+    List<Account> matches = [];
+
+    for (Account account in suggestions) {
+      RegExp exp = RegExp(pattern, caseSensitive: false);
+      if (exp.hasMatch(account.title)) {
+        matches.add(account);
+      }
+    }
+
+    return matches;
   }
 
   @override
@@ -69,64 +84,69 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Container(
-          padding: EdgeInsets.all(5),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(1000)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(0, 0, 0, 0.04),
-                  spreadRadius: 0,
-                  blurRadius: 10,
-                )
-              ]),
-          child: TypeAheadField(
-            textFieldConfiguration: TextFieldConfiguration(
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  suffixIcon: Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                  hintStyle: TextStyle(fontSize: 12, color: Colors.grey),
-                  hintText: 'www.faz.de/yourarticle, Nicky Minaj, Apple,...',
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.transparent,
-                          width: 0,
-                          style: BorderStyle.solid),
-                      borderRadius: BorderRadius.all(Radius.circular(1000))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.pink,
-                          width: 2.0,
-                          style: BorderStyle.solid),
-                      borderRadius: BorderRadius.all(Radius.circular(1000))),
-                )),
-            suggestionsCallback: (pattern) async {
-              return await getSuggestions();
-            },
-            itemBuilder: (context, suggestion) {
-              Account account = suggestion as Account;
-              return ListTile(
-                leading: Icon(Icons.search),
-                title: Text(account.title),
-                subtitle: Text(account.label),
-              );
-            },
-            onSuggestionSelected: (suggestion) {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => Container()));
-            },
+        padding: const EdgeInsets.only(top: 200),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Container(
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(1000)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.04),
+                    spreadRadius: 0,
+                    blurRadius: 10,
+                  )
+                ]),
+            child: TypeAheadField(
+              noItemsFoundBuilder: (context) {
+                return ListTile(
+                  leading: Icon(Icons.no_accounts),
+                  title: Text("No matches found"),
+                );
+              },
+              textFieldConfiguration: TextFieldConfiguration(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    suffixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
+                    hintText:
+                        'www.faz.de/yourarticle, Nicky Minaj, Apple, Benjamin Shapiro',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.transparent,
+                            width: 0,
+                            style: BorderStyle.solid),
+                        borderRadius: BorderRadius.all(Radius.circular(1000))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.pink,
+                            width: 2.0,
+                            style: BorderStyle.solid),
+                        borderRadius: BorderRadius.all(Radius.circular(1000))),
+                  )),
+              suggestionsCallback: (pattern) async {
+                return await getSuggestions(pattern);
+              },
+              itemBuilder: (context, suggestion) {
+                Account account = suggestion as Account;
+                return ListTile(
+                  leading: Icon(Icons.search),
+                  title: Text(account.title),
+                  subtitle: Text(account.label),
+                );
+              },
+              onSuggestionSelected: (suggestion) {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Container()));
+              },
+            ),
           ),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
